ef8ad4cc8e8f4812a32a5a550be5b7e1ee95e30f
dianatijeras <dianam.garciaa1@uqvirtual.edu.co>
2026-05-05 09:36:46 -0500

    feat: creacion proyecto

---
+++ ProyectoFinalPrueba/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.sistemahotelprueba.proyectofinalprueba {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.sistemahotelprueba.proyectofinalprueba to javafx.fxml;
+    exports co.edu.uniquindio.sistemahotelprueba.proyectofinalprueba;
+}
